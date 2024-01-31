deef562fdd26892a54166d6ed7fd92998492a37d
Weblazo <example@gmail.com>
2024-01-31 21:07:08 +0200

    Conditions

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,38 @@
 "use strict"
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", " ");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", " ");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt("Один из последних просмотренных фильмов?", " "),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", " "),
-      d = prompt("На сколько оцените его?", "");
+// const a = prompt("Один из последних просмотренных фильмов?", " "),
+//       b = prompt("На сколько оцените его?", ""),
+//       c = prompt("Один из последних просмотренных фильмов?", " "),
+//       d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 5:
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
